4ebf98bec7d969f40119ee8dc4568b5982c6aaba
James Nocentini <james.nocentini@gmail.com>
2013-02-19 14:48:25 +0000

    specified the engine dependency in package.json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8000;
-app.listen(port);
 
 app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
